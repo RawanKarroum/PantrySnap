9203e351619d6769ac77fa4788b49e9e45c2644e
Rawan Karroum <rawan.r.karroum@gmail.com>
2024-08-04 13:29:18 -0400

    google analytics

--- pantry-snap/src/app/layout.tsx
+++ pantry-snap/src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import { Inter } from 'next/font/google';
 import Head from 'next/head'; 
+import Script from 'next/script'; 
 import './globals.css';
 import { FirebaseAppProvider } from 'reactfire';
 import { firebaseConfig } from './config/Firebase';
@@ -18,6 +19,23 @@ export default function RootLayout({
     <FirebaseAppProvider firebaseConfig={firebaseConfig}>
       <UserProvider>
         <html lang="en">
+          <head>
+            <Script
+              src="https://www.googletagmanager.com/gtag/js?id=G-P74S6R8LE6"
+              strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-P74S6R8LE6', {
+                  page_path: window.location.pathname,
+                });
+              `}
+            </Script>
+          </head>
           <body className={inter.className} style={{ overflow: 'hidden', height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
             {children}
           </body>
